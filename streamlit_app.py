ad590775b2080f3a423ac9be0f16c89f9227f8e5
joaquinwessolovski-cmd <joaquinwessolovski@gmail.com>
2025-10-04 12:06:34 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ CLASICOS = {
     "Clásico de Avellaneda": ("Racing Club", "Independiente"),
     "Clásico de Boedo": ("San Lorenzo", "Huracán"),
     "Clásico Rosarino": ("Rosario Central", "Newell's OB"),
-    "Clásico Platense": ("Gimnasia–LP", "Estudiantes-LP"),
+    "Clásico Platense": ("Gimnasia–LP", "Estudiantes–LP"),
     "Clásico Cordobes": ("Belgrano", "Talleres"),
     "Clásico Santafesino": ("Colón", "Unión"),
     "Clásico de Zona Norte": ("Tigre", "Platense"),
